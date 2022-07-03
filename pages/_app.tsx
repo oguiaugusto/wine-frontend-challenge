63fdb2970ca3a93ebac70e7f10ece6439d37fee2
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-07-02 21:45:44 -0300

    chore: create search bar animations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { AppProps } from 'next/app';
 import ProductsProvider from '../context/ProductsContext';
 import '../styles/globals.css';
+import '../styles/animations.css';
 
 const App = ({ Component, pageProps }: AppProps) => (
   <ProductsProvider>
