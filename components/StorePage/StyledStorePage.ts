1b530702c7d2a3e4b8b637c63cf0bd846bccc6aa
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-07-03 17:23:15 -0300

    feat: add filter per price section
    not functional yet, but styled for big screens only

--- components/StorePage/StyledStorePage.ts
+++ components/StorePage/StyledStorePage.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 const StyledStorePage = styled.div`
+  .filters {
+    display: none;
+  }
+
   .found-products-amount {
     color: #888888;
     font-style: normal;
@@ -53,6 +57,11 @@ const StyledStorePage = styled.div`
   @media screen and (min-width: 992px) {
     padding: 39px;
 
+    .products-section {
+      margin: auto;
+      width: 832px
+    }
+
     .found-products-amount {
       color: #262626;
       border: none;
@@ -65,7 +74,7 @@ const StyledStorePage = styled.div`
       grid-template-columns: repeat(3, 1fr);
       grid-auto-rows: 1fr;
       grid-column-gap: 32px;
-      width: 832px;
+      width: 100%;
       margin: auto;
     }
   
@@ -76,6 +85,93 @@ const StyledStorePage = styled.div`
     .products .add-to-cart {
       width: 256px;
     }
+  }
+
+  @media screen and (min-width: 1212px) {
+
+    display: flex;
+    justify-content: space-between;
+    padding: 39px 50px;
+  
+    .filters {
+      display: block;
+      /* margin-right: 32px; */
+    }
+
+    .filters .title {
+      font-family: Titillium Web, sans-serif;
+      font-style: normal;
+      font-weight: 700;
+      font-size: 20px;
+      margin: -6px 0 23px;
+    }
+
+    .filters .per-price {
+      display: flex;
+      flex-direction: column;
+      user-select: none;
+    }
+    
+    .filters .per-price p {
+      color: #333333;
+      font-style: normal;
+      font-size: 18px;
+      font-weight: 700;
+      margin-bottom: 25px;
+    }
+
+    .filters .per-price label {
+      color: #1d1d1b;
+      font-style: normal;
+      font-weight: 400;
+      font-size: 14px;
+      margin-bottom: 24px;
+      display: flex;
+      align-items: center;
+      cursor: pointer;
+    }
+
+    .filters .per-price label input {
+      margin-right: 12px;
+      accent-color: #888888;
+    }
+
+    .filters .per-price label input[type=radio] {
+      transform: scale(1.5);
+    }
+
+    .filters .per-price label input[type=radio]:after {
+      content: '';
+      position: relative;
+      display: inline-block;
+      width: 12px;
+      height: 12px;
+      border-radius: 50%;
+      top: -1.8px;
+      left: -2px;
+      background-color: #f5f5f5;
+      border: 2px solid #888888;
+      cursor: pointer;
+    }
+
+    .filters .per-price label input[type=radio]:hover:after {
+      border: 2px solid #707070;
+    }
+
+    .filters .per-price label input[type=radio]:checked:after {
+      background-color: #888888;
+    }
+
+    .products-section {
+      margin: 0;
+    }
+  }
+
+  @media screen and (min-width: 1440px) {
+  
+    justify-content: center;
+    gap: 125px;
+    padding: 39px 160px;
   }
 `;
 
--- pages/loja/index.tsx
+++ pages/loja/index.tsx
@@ -30,36 +30,60 @@ const index: NextPage<PageProps> = ({ productsResponse }) => {
     }
   }, [productsResponse]);
 
+  const perPriceFilters = [
+    { id: 'unitl-40', from: 0, to: 40, label: 'Até R$40' },
+    { id: '40-to-60', from: 41, to: 60, label: 'R$40 A R$60' },
+    { id: '100-to-200', from: 61, to: 200, label: 'R$100 A R$200' },
+    { id: '200-to-500', from: 201, to: 500, label: 'R$ A R$500' },
+    { id: 'over-500', from: 501, label: 'Acima de R$500' },
+  ];
+
   return (
     <StyledStorePage>
-      <p className="found-products-amount">
-        <span className="amount">{productsResponse?.totalItems}</span>
-        <span>{ ' produtos encontrados' }</span>
-      </p>
-      <div className="products">
-        {
-          c?.products.map((product) => (
-            <div key={ `product-${product.id}` } className="product">
-              <ProductCard product={ product } />
-              <AddToCart />
-            </div>
-          ))
-        }
-      </div>
-      <button
-        type="button"
-        disabled={ previousPageDisabled }
-        onClick={ () => router.push(`/loja?page=${previousPage}`) }
-      >
-        Previous
-      </button>
-      <button
-        type="button"
-        disabled={ nextPageDisabled }
-        onClick={ () => router.push(`/loja?page=${nextPage}`) }
-      >
-        Next
-      </button>
+      <section className="filters">
+        <p className="title">Refine sua busca</p>
+        <div className="per-price">
+          <p>Por preço</p>
+          {
+            perPriceFilters.map(({ id, label, from, to }) => (
+              <label key={ `price-filter-${id}` } htmlFor={ id }>
+                <input type="radio" name="per-price-filter" id={ id } onClick={ () => { ''; } } />
+                {label}
+              </label>
+            ))
+          }
+        </div>
+      </section>
+      <section className="products-section">
+        <p className="found-products-amount">
+          <span className="amount">{productsResponse?.totalItems}</span>
+          <span>{ ' produtos encontrados' }</span>
+        </p>
+        <div className="products">
+          {
+            c?.products.map((product) => (
+              <div key={ `product-${product.id}` } className="product">
+                <ProductCard product={ product } />
+                <AddToCart />
+              </div>
+            ))
+          }
+        </div>
+        <button
+          type="button"
+          disabled={ previousPageDisabled }
+          onClick={ () => router.push(`/loja?page=${previousPage}`) }
+        >
+          Previous
+        </button>
+        <button
+          type="button"
+          disabled={ nextPageDisabled }
+          onClick={ () => router.push(`/loja?page=${nextPage}`) }
+        >
+          Next
+        </button>
+      </section>
     </StyledStorePage>
   );
 };
