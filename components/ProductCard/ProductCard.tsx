afe481349d723cbbc6ddc43687aa109c2ac33b50
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-07-03 13:39:40 -0300

    chore: create product card component styled for small screens

---
+++ components/ProductCard/ProductCard.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { IProduct } from '../../interfaces/IProduct';
+import toCommaFixed from '../../utils/toCommaFixed';
+import StyledProductCard from './StyledProductCard';
+
+interface ProductCardProps {
+  product: IProduct;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const offPercentage = 2;
+
+  return (
+    <StyledProductCard>
+      <div className="picture">
+        <img src={ product.image } alt={ product.name } className="product-image" />
+        <img src="/images/black-wine-label.svg" alt="Selo Black Wine" className="label" />
+      </div>
+      <p className="title">{product.name}</p>
+      <div className="price">
+        <p className="standart">{ `R$ ${toCommaFixed(product.price, 2)} `}</p>
+        <p className="off-percentage">{ `${offPercentage}% OFF` }</p>
+      </div>
+      <div className="price-member">
+        <p className="member-wine">SÓCIO WINE</p>
+        <p className="member-value">
+          <span className="sign">{ 'R$ ' }</span>
+          <span className="value">{toCommaFixed(product.priceMember, 2)}</span>
+        </p>
+      </div>
+      <p className="price-non-member">{ `NÃO SÓCIO R$ ${toCommaFixed(product.priceNonMember, 2)}` }</p>
+    </StyledProductCard>
+  );
+};
+
+export default ProductCard;
---
+++ components/ProductCard/StyledProductCard.ts
@@ -0,0 +1,107 @@
+import styled from 'styled-components';
+
+const StyledProductCard = styled.div`
+  background-color: #fff;
+  font-style: normal;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  width: 156px;
+
+  :hover {
+    box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.3);
+  }
+
+  .picture {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    height: 181px;
+    margin: 16px 16px 0;
+  }
+
+  .picture .product-image {
+    height: 100%;
+  }
+
+  .picture .label {
+    position: absolute;
+    bottom: 1px;
+    left: -8px;
+  }
+
+  .title {
+    margin: 12px 8px;
+    font-weight: 700;
+    font-size: 14px;
+    text-align: center;
+  }
+
+  .price {
+    font-weight: 400;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    margin-bottom: 4px;
+  }
+
+  .price .standart {
+    color: #888888;
+    text-decoration-line: line-through;
+    text-transform: uppercase;
+    font-size: 10px;
+  }
+
+  .price .off-percentage {
+    background-color: #f79552;
+    color: #ffffff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 12px;
+    width: 65px;
+    border-radius: 2px;
+  }
+
+  .price-member {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+  }
+
+  .price-member .member-wine {
+    color: #555555;
+    text-transform: uppercase;
+    text-align: right;
+    font-weight: 300;
+    font-size: 10px;
+    width: 32px;
+  }
+
+  .price-member .member-value {
+    color: #c81a78;
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 28px;
+  }
+
+  .price-member .member-value .sign {
+    font-size: 10px;
+  }
+
+  .price-non-member {
+    color: #888888;
+    font-weight: 400;
+    font-size: 10px;
+    align-self: center;
+    margin: 8px 0; 
+  }
+`;
+
+export default StyledProductCard;
